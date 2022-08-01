5825490a6443b6de897db0646adf53a497be33e5
Callum Axon <git@callumaxon.uk>
2022-08-01 08:35:16 +0100

    [tabulator-tables] add list editor type for table row
    In version 5.2 of tabulator-tables, the list editor type was added and `select` & `autocomplete` deprecated. REF: http://tabulator.info/docs/5.2/release#edit

--- types/tabulator-tables/index.d.ts
+++ types/tabulator-tables/index.d.ts
@@ -1597,6 +1597,7 @@ declare namespace Tabulator {
         | 'star'
         | 'select'
         | 'autocomplete'
+        | 'list'
         | ((
             cell: CellComponent,
             onRendered: EmptyCallback,
